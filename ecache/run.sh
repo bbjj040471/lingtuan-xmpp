d3efc20c22808b3f0aa9d6a5882ae51e36bd1045
root <root@yuejian.com>
2015-04-02 14:33:26 +0800

    modified:   ejabberd/v2.1.13/src/aa_group_chat.erl 	modified:   ejabberd/v2.1.13/src/aa_http.erl 	new file:   ejabberd/v2.1.13/src/aa_mongo_senter.erl 	new file:   ejabberd/v2.1.13/src/aa_mongo_senter_sup.erl 	new file:   ejabberd/v2.1.13/src/aa_mongo_sup.erl 	new file:   ejabberd/v2.1.13/src/aa_mongodb.erl 	modified:   ejabberd/v2.1.13/src/aa_packet_filter.erl 	new file:   ejabberd/v2.1.13/src/bson.erl 	new file:   ejabberd/v2.1.13/src/bson_binary.erl 	new file:   ejabberd/v2.1.13/src/bson_binary.hrl 	new file:   ejabberd/v2.1.13/src/bson_schema.erl 	modified:   ejabberd/v2.1.13/src/config.log 	modified:   ejabberd/v2.1.13/src/config.status 	modified:   ejabberd/v2.1.13/src/ejabberd.init 	modified:   ejabberd/v2.1.13/src/ejabberd_router.erl 	modified:   ejabberd/v2.1.13/src/ejabberd_sup.erl 	modified:   ejabberd/v2.1.13/src/ejabberd_zlib/Makefile 	modified:   ejabberd/v2.1.13/src/eldap/Makefile 	new file:   ejabberd/v2.1.13/src/mc_action_man.erl 	new file:   ejabberd/v2.1.13/src/mc_auth.erl 	new file:   ejabberd/v2.1.13/src/mc_connection_man.erl 	new file:   ejabberd/v2.1.13/src/mc_cursor.erl 	new file:   ejabberd/v2.1.13/src/mc_cursor_sup.erl 	new file:   ejabberd/v2.1.13/src/mc_super_sup.erl 	new file:   ejabberd/v2.1.13/src/mc_utils.erl 	new file:   ejabberd/v2.1.13/src/mc_worker.erl 	new file:   ejabberd/v2.1.13/src/mc_worker_logic.erl 	modified:   ejabberd/v2.1.13/src/mod_irc/Makefile 	modified:   ejabberd/v2.1.13/src/mod_muc/Makefile 	modified:   ejabberd/v2.1.13/src/mod_proxy65/Makefile 	modified:   ejabberd/v2.1.13/src/mod_pubsub/Makefile 	new file:   ejabberd/v2.1.13/src/mongo.erl 	new file:   ejabberd/v2.1.13/src/mongo_id_server.erl 	new file:   ejabberd/v2.1.13/src/mongo_pool.erl 	new file:   ejabberd/v2.1.13/src/mongo_protocol.erl 	new file:   ejabberd/v2.1.13/src/mongo_protocol.hrl 	copied:     ejabberd/v2.1.13/src/mod_proxy65/Makefile -> ejabberd/v2.1.13/src/odbc/Makefile 	modified:   ejabberd/v2.1.13/src/stringprep/Makefile 	modified:   ejabberd/v2.1.13/src/stun/Makefile 	modified:   ejabberd/v2.1.13/src/tls/Makefile 	modified:   ejabberd/v2.1.13/src/web/Makefile

--- ecache/run.sh
+++ ecache/run.sh
@@ -1,3 +1,3 @@
 #erl -sname ecache@localhost -config ecache.config -noshell -pa ebin/ -pa deps/*/ebin/ -s ecache_run start & 
-erl -sname ecache@localhost -config ecache.config -pa ebin/ -pa deps/*/ebin/ -s ecache_run start
-
+#erl -sname ecache@localhost -config ecache.config -pa ebin/ -pa deps/*/ebin/ -s ecache_run start
+erl -noshell -name ecache@10.247.2.78 +K true +P 300000 -smp auto -env ERL_MAX_PORTS 60000 -config ecache.config -pa ebin/ -pa deps/*/ebin/ -s ecache_run start &
---
+++ ejabberd/v2.1.13/src/aa_mongodb.erl
@@ -0,0 +1,310 @@
+%% @author Administrator
+%% @doc @todo Add description to aa_mongodb.
+
+
+-module(aa_mongodb).
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-include("ejabberd.hrl").
+-include("jlib.hrl").
+
+-define(OPERATE_USERLIST,"operate_userlist").
+%% -define(OPERATE_GROUPLIST,"operate_grouplist").
+%% -define(OPERATE_SUPERGROUPLIST,"operate_supergrouplist").
+
+-define(SYSN_TIME,20*1000).
+
+-define(MONGO_POOL,mongo_pool).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start_link/0,
+		save_mongo/3,
+		set_opt_userlist/0
+%% 		set_opt_grouplist/1,
+%% 		set_opt_supergrouplist/1
+		]).
+
+start_link()->
+	gen_server:start_link({local,?MODULE}, ?MODULE, [], []).
+
+
+
+%% ====================================================================
+%% Behavioural functions 
+%% ====================================================================
+-record(state, {msgdict,userlist}).
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:init-1">gen_server:init/1</a>
+-spec init(Args :: term()) -> Result when
+	Result :: {ok, State}
+			| {ok, State, Timeout}
+			| {ok, State, hibernate}
+			| {stop, Reason :: term()}
+			| ignore,
+	State :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+
+
+init([]) ->
+	erlang:send_after(8000, ?MODULE, sysn_mongo),
+	case aa_hookhandler:ecache_cmd(["GET",?OPERATE_USERLIST]) of
+		{ok,undefined}->
+			UserList = [];
+		{ok,Bin} when is_binary(Bin)->
+			UserList = lists:filter(fun(Uid)-> erlang:is_integer(Uid) end, erlang:binary_to_term(Bin));
+		_->
+			UserList = []
+	end,
+	?WARNING_MSG("sysn_mongo state:~p",[UserList]),
+    {ok, #state{msgdict = [],userlist = UserList}}.
+
+save_mongo(From, To, Packet)->
+	try
+	    case Packet of 
+	    	{_,"message",_Attr,_} ->
+				gen_server:cast(?MODULE, {save_mongo,From, To,Packet});
+			_->
+				skip
+		end
+	catch
+		E:R->
+			?ERROR_MSG("Error happen at ~p :save_mongo()reason:~p",[?MODULE,{E,R}])
+	end.
+
+%%设置运营账户。
+set_opt_userlist()->
+	Userlist = aa_mongo_senter:read_opt_uidlist(),
+	aa_hookhandler:ecache_cmd(["SET",?OPERATE_USERLIST,erlang:term_to_binary(Userlist)]),
+	gen_server:call(?MODULE, {reset_opt,{userlist,Userlist}}).
+
+%% handle_call/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_call-3">gen_server:handle_call/3</a>
+-spec handle_call(Request :: term(), From :: {pid(), Tag :: term()}, State :: term()) -> Result when
+	Result :: {reply, Reply, NewState}
+			| {reply, Reply, NewState, Timeout}
+			| {reply, Reply, NewState, hibernate}
+			| {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason, Reply, NewState}
+			| {stop, Reason, NewState},
+	Reply :: term(),
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+handle_call({reset_opt,{Type,Data}},_from,State)->
+	if
+		Type =:= userlist ->
+			NewState = State#state{userlist = Data};
+		true->
+			NewState = State
+	end,
+	{reply,ok,NewState};
+
+handle_call(_Request, _From, State) ->
+    Reply = error,
+    {reply, Reply, State}.
+
+
+%% handle_cast/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_cast-2">gen_server:handle_cast/2</a>
+-spec handle_cast(Request :: term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_cast({save_mongo,#jid{luser = Fromstr} = _FromJid, #jid{luser = Tostr} = ToJid,Packet},State)->
+	
+	Mid = xml:get_tag_attr_s("id", Packet),
+	Type = xml:get_tag_attr_s("type", Packet),
+	MsgType = xml:get_tag_attr_s("msgtype", Packet),
+	OldMsgTime = xml:get_tag_attr_s("msgTime", Packet),
+	if
+		OldMsgTime =/= ""->
+			MsgTime = OldMsgTime;
+		true->
+			{M,S,SS} = now(), 
+			MsgTime = lists:sublist(erlang:integer_to_list(M*1000000000000+S*1000000+SS),1,13)
+	end,
+	case MsgType of
+		"normalchat"->
+			From = erlang:list_to_integer(Fromstr),
+			To = erlang:list_to_integer(Tostr),
+			Result =  lists:member(To, State#state.userlist),
+			FResult = lists:member(From, State#state.userlist),
+			if
+				Result =:= false,FResult =:= false->
+					NewState = State;
+				true->
+					[JSON] = aa_log:get_text_message_from_packet(Packet),
+					DbMsg = {
+							 id,list_to_binary(Mid),
+							 from,From,
+							 to,To,
+							 type,list_to_binary(Type),
+							 msgtype,list_to_binary(MsgType),
+							 msgTime,erlang:list_to_integer(MsgTime),
+							 body,erlang:list_to_binary(JSON),
+							 isread,0
+							 },
+					NewState = State#state{msgdict = [DbMsg|State#state.msgdict]}
+			end;
+		"groupchat"->
+				From = erlang:list_to_integer(Fromstr),
+				To = erlang:list_to_integer(Tostr),
+				case aa_group_chat:is_group_chat(ToJid) of
+					false->
+						Result =  lists:member(To, State#state.userlist);
+					true->
+						Result = lists:member(From, State#state.userlist)
+				end,
+				if
+					Result =:= false->
+						NewState = State;
+					true->
+						[JSON] = aa_log:get_text_message_from_packet(Packet),
+						DbMsg = {
+								 id,list_to_binary(Mid),
+								 from,From,
+								 to,To,
+								 type,list_to_binary(Type),
+								 msgtype,list_to_binary(MsgType),
+								 msgTime,erlang:list_to_integer(MsgTime),
+								 body,erlang:list_to_binary(JSON),
+								 isread,0
+								 },
+						NewState = State#state{msgdict = [DbMsg|State#state.msgdict]}
+				end;
+		"super_groupchat"->
+					From = erlang:list_to_integer(Fromstr),
+					To = erlang:list_to_integer(Tostr),
+					case aa_group_chat:is_group_chat(ToJid) of
+						false->
+							Result =  lists:member(To, State#state.userlist);
+						true->
+							Result = lists:member(From, State#state.userlist)
+					end,
+					if
+						Result =:= false->
+							NewState = State;
+						true->
+							[JSON] = aa_log:get_text_message_from_packet(Packet),
+							DbMsg = {
+									 id,list_to_binary(Mid),
+									 from,From,
+									 to,To,
+									 type,list_to_binary(Type),
+									 msgtype,list_to_binary(MsgType),
+									 msgTime,erlang:list_to_integer(MsgTime),
+									 body,erlang:list_to_binary(JSON),
+									 isread,0
+									 },
+							NewState = State#state{msgdict = [DbMsg|State#state.msgdict]}
+					end;
+		"system"->
+			To = erlang:list_to_integer(Tostr),
+			From = erlang:list_to_integer(Fromstr),
+			case aa_group_chat:is_group_chat(ToJid) of
+				false->
+					Result =  lists:member(To, State#state.userlist);
+				true->
+					Result = lists:member(From, State#state.userlist)
+			end,
+			if
+				Result=:= false->
+					NewState = State;
+				true->
+					[JSON] = aa_log:get_text_message_from_packet(Packet),
+					DbMsg = {
+							 id,list_to_binary(Mid),
+							 from,From,
+							 to,To,
+							 type,list_to_binary(Type),
+							 msgtype,list_to_binary(MsgType),
+							 msgTime,erlang:list_to_integer(MsgTime),
+							 body,erlang:list_to_binary(JSON),
+							 isread,0
+							 },
+					NewState = State#state{msgdict = [DbMsg|State#state.msgdict]}
+			end;
+		_->
+			NewState = State
+	end,
+	{noreply, NewState};
+					
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+
+%% handle_info/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_info-2">gen_server:handle_info/2</a>
+-spec handle_info(Info :: timeout | term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_info(sysn_mongo,State)->
+	?INFO_MSG("sysn_mongo state:~p",[State]),
+	aa_mongo_senter:sysn_write(State#state.msgdict),
+	case aa_hookhandler:ecache_cmd(["GET",?OPERATE_USERLIST]) of
+		{ok,undefined}->
+			UserList = [];
+		{ok,Bin} when is_binary(Bin)->
+			UserList = lists:filter(fun(Uid)-> erlang:is_integer(Uid) end, erlang:binary_to_term(Bin));
+		_->
+			UserList = []
+	end,
+	NewState = State#state{msgdict = [],userlist = UserList},
+	erlang:send_after(?SYSN_TIME, ?MODULE, sysn_mongo),
+	{noreply, NewState};
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+
+%% terminate/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:terminate-2">gen_server:terminate/2</a>
+-spec terminate(Reason, State :: term()) -> Any :: term() when
+	Reason :: normal
+			| shutdown
+			| {shutdown, term()}
+			| term().
+%% ====================================================================
+terminate(_Reason, _State) ->
+    ok.
+
+
+%% code_change/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:code_change-3">gen_server:code_change/3</a>
+-spec code_change(OldVsn, State :: term(), Extra :: term()) -> Result when
+	Result :: {ok, NewState :: term()} | {error, Reason :: term()},
+	OldVsn :: Vsn | {down, Vsn},
+	Vsn :: term().
+%% ====================================================================
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
